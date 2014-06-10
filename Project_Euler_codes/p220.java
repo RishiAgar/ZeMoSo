c8eba5fd1d854f66e9f997209b7744891fe9d095
RishiAgar <rishi.agarwal1992@gmail.com>
2014-06-10 16:44:02 +0530

    P220

--- Project_Euler_codes/p220.java
+++ Project_Euler_codes/p220.java
@@ -1,171 +1,68 @@
-
 package mine;
-
-import java.util.*;
 
 public class p220 
 {
-    public static class Point
-    {
-        long x=0;
-        long y=0;
-        byte quad=1;
-    }
-    public Point func(Point a)
-    {
-        
-        return a;
-    }
     public static void main(String args[])
     {
-        int i;
-        Point[] a = new Point[50];
-        for( i=0; i<50; i++)
-            a[i] = new Point();
-        Point[] b = new Point[50];
-        for( i=0; i<50; i++)
-            b[i] = new Point();
-        a[1].x = 1;
-        a[1].y = 1;
-        a[1].quad = 1;
-        i=1;
-        while( i<45)
+        long[] X = new long[42];
+        long[] Y = new long[42];
+        long[] completeSteps = new long[42];
+	int i = 2, dir = 0;
+        long x = 0, y = 1, steps, maxSteps, temp_x, temp_y, pos_x = 0, pos_y = 0;
+	maxSteps = (long)Math.pow(10,12);
+	steps = 1;
+	X[1] = x; 
+        Y[1] = y; 
+        completeSteps[1] = 1;
+	while ( steps < maxSteps)
         {
-            if( a[i].quad == 1)
-            {
-                a[i+1].x = a[i].x + a[i].y;
-                a[i+1].y = 0;
-                a[i+1].quad = 2;
-            }
-            else if( a[i].quad == 2)
-            {
-                a[i+1].y = -a[i].x;
-                a[i+1].x = a[i].x;
-                a[i+1].quad = 3;
-            }
-            else if( a[i].quad == 3)
-            {
-                a[i+1].x = 0;
-                a[i+1].y = a[i].y*2;
-                a[i+1].quad = 4;
-            }
-            else if( a[i].quad == 4)
-            {
-                a[i+1].x = a[i].y ;
-                a[i+1].y = a[i].y;
-                a[i+1].quad = 5;
-            }
-            else if( a[i].quad == 5)
-            {
-                a[i+1].x = a[i].x*2;
-                a[i+1].y = 0;
-                a[i+1].quad = 6;
-            }
-            else if( a[i].quad == 6)
-            {
-                a[i+1].y = -a[i].x;
-                a[i+1].x = a[i].x;
-                a[i+1].quad = 7;
-            }
-            else if( a[i].quad == 7)
-            {
-                a[i+1].x = 0;
-                a[i+1].y = a[i].y*2;
-                a[i+1].quad = 8;
-            }
-            else if( a[i].quad == 8)
-            {
-                a[i+1].x = a[i].y;
-                a[i+1].y = a[i].y;
-                a[i+1].quad = 1;
-            }
-            System.out.println(i +" " + a[i].quad+" "+Math.pow(2, i) + "  " + a[i].x + " , " + a[i].y);
-            i++;
-            //System.out.println(i +" " + a[i].quad+" "+Math.pow(2, i) + "  " + a[i].x + " , " + a[i].y);
-        }
-        b[1].x = -1;
-        b[1].y = -1;
-        b[1].quad = 5;
-        i=1;
-        while( i<45)
+		temp_x = x; 
+                temp_y = y;
+		x = temp_x + temp_y;
+		y = temp_y - temp_x;
+		steps = steps + steps;
+		X[i] = x; 
+                Y[i] = y; 
+                completeSteps[i] = steps; 
+                i++;
+	}
+        x = 1; 
+        y = 1; 
+        i--;
+	while( maxSteps > 0 )
         {
-            if( b[i].quad == 1)
+            if( maxSteps > completeSteps[i])
             {
-                b[i+1].x = b[i].x + b[i].y;
-                b[i+1].y = 0;
-                b[i+1].quad = 2;
+                switch(dir)
+                {
+                    case 0: 
+                        x = X[i+1]; 
+                        y = Y[i+1]; 
+                        dir = 3;
+                        break;
+                    case 1: 
+                        x = Y[i+1]; 
+                        y =-X[i+1];
+                        dir = 0;
+                        break;
+                    case 2: 
+                        x =- X[i+1]; 
+                        y =- Y[i+1]; 
+                        dir = 1;
+                        break;
+                    case 3: 
+                        x =- Y[i+1]; 
+                        y = X[i+1];
+                        dir = 2;
+                        break;
+                }
+                pos_x += x; 
+                pos_y += y;
+                maxSteps = completeSteps[i+1] - maxSteps;
             }
-            else if( b[i].quad == 2)
-            {
-                b[i+1].y = -b[i].x;
-                b[i+1].x = b[i].x;
-                b[i+1].quad = 3;
-            }
-            else if( b[i].quad == 3)
-            {
-                b[i+1].x = 0;
-                b[i+1].y = b[i].y*2;
-                b[i+1].quad = 4;
-            }
-            else if( b[i].quad == 4)
-            {
-                b[i+1].x = b[i].y ;
-                b[i+1].y = b[i].y;
-                b[i+1].quad = 5;
-            }
-            else if( b[i].quad == 5)
-            {
-                b[i+1].x = b[i].x*2;
-                b[i+1].y = 0;
-                b[i+1].quad = 6;
-            }
-            else if( b[i].quad == 6)
-            {
-                b[i+1].y = -b[i].x;
-                b[i+1].x = b[i].x;
-                b[i+1].quad = 7;
-            }
-            else if( b[i].quad == 7)
-            {
-                b[i+1].x = 0;
-                b[i+1].y = b[i].y*2;
-                b[i+1].quad = 8;
-            }
-            else if( b[i].quad == 8)
-            {
-                b[i+1].x = b[i].y;
-                b[i+1].y = b[i].y;
-                b[i+1].quad = 1;
-            }
-            System.out.println(i +" " + b[i].quad+" "+Math.pow(2, i) + "  " + b[i].x + " , " + b[i].y);
-            i++;
-        }
-        i=40;
-        long maxSteps = (long) Math.pow(10,12);
-        maxSteps -= (long)Math.pow(2, i-1);
-        Point pos = new Point();
-        pos.x= a[i-1].x;
-        pos.y = a[i-1].y;
-        System.out.println("--------------------"+pos.x+" "+pos.y + " " + maxSteps);
-        i=i-2;
-        while( true)
-        {
-            maxSteps -= (long)Math.pow(2, i);
-            if( maxSteps>=0)
-                System.out.println(pos.x+" "+pos.y + " " + maxSteps +" " + i );
-            if( maxSteps < 0 )
-            {
-                maxSteps += (long)Math.pow(2,i);
-                i--;
-                if( i==12)
-                    i--;
-                continue;
-            }
-            pos.x +=b[i].x;
-            pos.y += b[i].y;
-            if( maxSteps == 0 )
-                break;
-        }
-        System.out.println(pos.x + "," + pos.y +" "+i);
+            i--;
+	}
+        System.out.println( pos_x + "," + pos_y);
     }
 }
+    
