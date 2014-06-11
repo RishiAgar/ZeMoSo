cfd63693a1f14285d8be70c682e97d9d552803c3
RishiAgar <rishi.agarwal1992@gmail.com>
2014-06-11 13:10:30 +0530

    P26

--- Project_Euler_codes/p26.java
+++ Project_Euler_codes/p26.java
@@ -1,30 +1,51 @@
-public class p26
+
+package mine;
+
+public class p26 
 {
-	public static void main(String args[])
-	{
-		int i, temp=0, count=0;
-		int[] a=new int[1001];
-		for( i=0; i<1001; i++ )
-			a[i]=0;
-		for( i=1; i<1001; i++ )
-		{
-			temp = i;
-			while( true )
-			{
-				if ( temp%2 !=0 && temp%5 != 0 )
-					break;
-				if( temp%2 == 0) 
-					temp/=2;
-				if( temp%5 == 0) 
-					temp/=5;
-			}
-			if(temp==i)
-				count++;
-			a[temp]++;
-		}
-		for( i=0; i<1001; i++ )
-			if( a[i] != 0)
-				System.out.print( i+" ");
-		System.out.println("\n"+count);
-	}
+    public static void main( String args[] )
+    {
+        int k, temp, d=0, max =0;
+        int[][] a = new int[2][1001];
+        for( int i=11; i<1000; i++ )
+        {
+            if( i%2 == 0 || i%5 == 0 )
+                continue;
+            else
+            {
+                for( int j=0; j<1001; j++ )
+                {
+                    a[0][j]=0;
+                    a[1][j]=0;
+                }
+                k=0;
+                int c=0;
+                if( i<100)
+                    temp = 100;
+                else
+                    temp = 1000;
+                while( true )
+                {
+                    k++;
+                    if( temp < i )
+                    {
+                        temp *= 10;
+                        continue;
+                    }
+                    temp = temp % i;
+                    if( a[0][temp] == 1)
+                        break;
+                    a[0][temp] = 1;
+                    a[1][temp] = k;
+                    temp *= 10;
+                }
+                if( max  < k )
+                {
+                    max = k;
+                    d = i;
+                }
+            }
+        }
+        System.out.println(d);
+    }
 }
